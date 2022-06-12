288a0a66cb4d0371801eb8be657de85de6ad90d3
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-12 21:07:26 +0300

    Completed

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Code your solution here
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-function findMatching(driver,about){
-    return driver.filter(driver=>{
-        return driver.toLowerCase()===about.toLowerCase();
+function findMatching(driversArray,about){
+    return driversArray.filter(item=>{
+        return item.toLowerCase()===about.toLowerCase();
     })
 }
-const fuzzyMatch=function(driver,about){
-   return driver.filter(driver=>{
-       return driver.toLowerCase().substring(0,about.length)===about.toLowerCase()
+const fuzzyMatch=function(drivers,about){
+   return drivers.filter(drivers=>{
+       return drivers.toLowerCase().substring(0,about.length)===about.toLowerCase()
    })
 }
-const matchName=(driver,about)=>{
-    return driver.filter(driver=>{
-        return driver.name===about;
+const matchName=(drivers,about)=>{
+    return drivers.filter(drivers=>{
+        return drivers.name===about;
     })
 }
